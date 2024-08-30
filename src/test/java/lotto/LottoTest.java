a7bda63296d98f7ba479f9c76ecf6db2ba48f291
minzzun99 <skg06123@naver.com>
2024-08-30 17:18:05 +0900

    test(purchase input): 로또 구매 금액 숫자 검증 테스트 작성

--- src/test/java/lotto/LottoTest.java
+++ src/test/java/lotto/LottoTest.java
@@ -2,6 +2,7 @@ package lotto;
 
 import lotto.domain.Lotto;
 import lotto.domain.RandomNumberGenerator;
+import lotto.exception.InputFormatValidator;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
@@ -28,12 +29,23 @@ class LottoTest {
     // 아래에 추가 테스트 작성 가능
     @DisplayName("로또 금액이 1,000으로 나누어떨어지지 않으면 예외가 발생한다.")
     @Test
-    void lottoPurchaseAmountDivideThousand() {
+    void amountCheckModZero() {
         RandomNumberGenerator randomNumberGenerator = new RandomNumberGenerator();
         int price = 2001;
 
         assertThatThrownBy(() -> randomNumberGenerator.generate(price))
                 .isInstanceOf(IllegalArgumentException.class)
                 .hasMessage("[ERROR] 로또 구매 금액이 1,000원으로 나누어 떨어지지 않습니다.");
+    }
+
+    @DisplayName("로또 금액에 숫자가 입력되지 않은 경우")
+    @Test
+    void amountCheckInputFormat() {
+        InputFormatValidator validator = new InputFormatValidator();
+        String invalidInput = "1000a"; // 숫자가 아닌 문자가 포함된 입력
+
+        assertThatThrownBy(() -> validator.purchaseInput(invalidInput))
+                .isInstanceOf(IllegalArgumentException.class)
+                .hasMessage("[ERROR] 로또 구매 금액은 숫자로 입력해주세요.");
     }
 }
