643dd8f73981e830c7ad0821af3e375667d1c1e9
minzzun99 <skg06123@naver.com>
2024-08-30 18:05:31 +0900

    feat(print): 에러메시지 출력하는 print 메서드 추가

--- src/main/java/lotto/view/OutputView.java
+++ src/main/java/lotto/view/OutputView.java
@@ -4,6 +4,14 @@ import lotto.view.message.LottoGameMessage;
 
 public class OutputView {
     public static void printMessage(LottoGameMessage message) {
-        System.out.println(message);
+        System.out.println(message.getMessage());
+    }
+
+    public static void printLottoCount(LottoGameMessage message, int lottoCount) {
+        System.out.println();
+    }
+
+    public static void printErrorMessage(String error) {
+        System.out.println(error + "\n");
     }
 }
