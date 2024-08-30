39225f01ba76306cf803205813e0a15abfc92172
minzzun99 <skg06123@naver.com>
2024-08-30 17:21:44 +0900

    refactor(generate): return type 변경

--- src/main/java/lotto/domain/RandomNumberGenerator.java
+++ src/main/java/lotto/domain/RandomNumberGenerator.java
@@ -9,13 +9,13 @@ import java.util.TreeSet;
 import lotto.exception.PurchaseAmountValidator;
 
 public class RandomNumberGenerator {
-    public List<List<Integer>> generate(int price) {
+    public List<Lotto> generate(int price) {
         // 검증
         new PurchaseAmountValidator(price);
         int generateCounts = price / LOTTO_PRICE.getNumber();
 
         // 랜덤 숫자 생성
-        List<List<Integer>> numbers = new ArrayList<>();
+        List<Lotto> numbers = new ArrayList<>();
         while (generateCounts-- > 0) {
             numbers.add(generateRandomNumbers());
         }
@@ -23,12 +23,12 @@ public class RandomNumberGenerator {
         return numbers;
     }
 
-    private List<Integer> generateRandomNumbers() {
+    private Lotto generateRandomNumbers() {
         TreeSet<Integer> numberSet = new TreeSet<>();
         while (numberSet.size() != NUMBER_COUNT.getNumber()) {
             numberSet.add(Randoms.pickNumberInRange(START_RANGE.getNumber(), END_RANGE.getNumber()));
         }
 
-        return new ArrayList<>(numberSet.stream().toList());
+        return new Lotto(new ArrayList<>(numberSet.stream().toList()));
     }
 }
