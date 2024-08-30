e12625ea986b70fc530a842e877f39fa1e25e441
minzzun99 <skg06123@naver.com>
2024-08-30 18:16:16 +0900

    feat(print): 구매한 로또를 출력하는 메서드 추가

--- src/main/java/lotto/view/OutputView.java
+++ src/main/java/lotto/view/OutputView.java
@@ -1,5 +1,8 @@
 package lotto.view;
 
+import static lotto.view.message.LottoGameMessage.*;
+
+import java.util.List;
 import lotto.view.message.LottoGameMessage;
 
 public class OutputView {
@@ -7,11 +10,17 @@ public class OutputView {
         System.out.println(message.getMessage());
     }
 
-    public static void printLottoCount(LottoGameMessage message, int lottoCount) {
-        System.out.println();
+    public static void printLottoCount(int lottoCount) {
+        StringBuilder sb = new StringBuilder();
+        sb.append('\n').append(lottoCount).append(LOTTO_COUNT.getMessage());
+        System.out.println(sb);
     }
 
     public static void printErrorMessage(String error) {
         System.out.println(error + "\n");
+    }
+
+    public static void printLottoList(List<Integer> lotto) {
+        System.out.println(lotto.toString());
     }
 }
