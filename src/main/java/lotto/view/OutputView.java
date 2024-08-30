4c3206cc84948003fe2c7e10b26e061420d9b639
minzzun99 <skg06123@naver.com>
2024-08-30 17:23:19 +0900

    feat(printMessage): 메시지 출력하는 OutputView 구현

---
+++ src/main/java/lotto/view/OutputView.java
@@ -0,0 +1,9 @@
+package lotto.view;
+
+import lotto.view.message.LottoGameMessage;
+
+public class OutputView {
+    public static void printMessage(LottoGameMessage message) {
+        System.out.println(message);
+    }
+}
